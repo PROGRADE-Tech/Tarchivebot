886e8705d44fcf8838e677befe93b10abd93bc2d
fuerbringer <severin@protonmail.ch>
2017-03-30 10:40:49 +0200

    Static HTTP server working

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 // Constants
 const PORT = 3000;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
-});
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+
+app.set('port', PORT);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var server = app.listen(app.get('port'), function() {
+	var port = server.address().port;
+	console.log('Serving static files from ./public to  localhost:' + port);
+});
