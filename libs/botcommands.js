d6b6fd3ac3b038d788a6e1cc19c88dc051d21cb3
fuerbringer <severin@protonmail.ch>
2017-03-31 15:43:50 +0200

    Preparations for telegram bot logger

--- libs/botcommands.js
+++ libs/botcommands.js
@@ -2,3 +2,23 @@
  * File: commands.js
  * Contains Telegram-Bot routines
  */
+const telegramBot = require('node-telegram-bot-api')
+const jsonFile = require('jsonfile')
+
+module.exports = {
+  run: function(configFile) {
+    if(configFile) {
+      jsonfile.readFile(configFile, function(err, obj) {
+        bot = new telegramBot(obj.token, { polling: true })
+
+        bot.onText(/.*/, function(msg, match) {
+          var chatId = msg.chat.id;
+          var resp = match[0];
+          // TODO
+          // Log message
+          // Insert into database
+        })
+      })
+    }
+  }
+}
