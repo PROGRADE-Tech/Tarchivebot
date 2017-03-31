58edccd7e0aab0c3156afc76e6f9c01fc58230f1
fuerbringer <severin@protonmail.ch>
2017-03-31 19:52:36 +0200

    Starting on implementing bot commands

--- libs/botcommands.js
+++ libs/botcommands.js
@@ -6,17 +6,32 @@ const telegramBot = require('node-telegram-bot-api')
 const jsonFile = require('jsonfile')
 
 module.exports = {
+
   run: function(configFile) {
     if(configFile) {
-      jsonfile.readFile(configFile, function(err, obj) {
+      jsonFile.readFile(configFile, function(err, obj) {
         bot = new telegramBot(obj.token, { polling: true })
 
         bot.onText(/.*/, function(msg, match) {
-          var chatId = msg.chat.id;
-          var resp = match[0];
+          var chatId = msg.chat.id
+          var resp = match[0]
+          //bot.sendMessage(chatId, "You said " + resp)
           // TODO
           // Log message
           // Insert into database
+        })
+
+        bot.onText(/\/start/, function(msg, match) {
+          var chatId = msg.chat.id
+          bot.sendMessage(chatId,
+            'Hello there! I am *Tarchivebot*. I log all your messages and calculate statistics.\nSelfhost it yourself for full privacy: https://github.com/fuerbringer/Tarchivebot',
+            { "parse_mode": "Markdown" }
+          )
+        })
+
+        bot.onText(/\/token/, function(msg, match) {
+          var chatId = msg.chat.id
+          // Provide SHA-256 token to for this chat (NOT the telegram token)
         })
       })
     }
