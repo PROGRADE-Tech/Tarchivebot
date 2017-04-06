4f314ba8eaa5563aefc8697bc43645bad2a3fc05
fuerbringer <severin@protonmail.ch>
2017-04-06 13:44:48 +0200

    Partial argument logic

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const bot = require('./libs/botcommands')
 const webserver = require('./libs/webserver')
 const api = require('./libs/api')
 
-// Part 1: run the telegram bot (and collect data)
-//bot.run('config/auth.json')
-
-// Part 2: serve the data
-webserver.serve()
-
-// Part 3: provide api
-api.serve()
+if(process.argv.length > 2) {
+	process.argv.forEach((val, index, array) => {
+		switch(val) {
+			case '--bot':
+				bot.run('config/auth.json') // run bot to log chats
+				break;
+			case '--webserver':
+				webserver.serve()	// serve the data
+				api.serve()				// provide api
+				break;
+		}
+	});
+} else {
+	// No specific arguments have been passed; run all scripts:
+	bot.run('config/auth.json') // run bot to log chats
+	webserver.serve()	// serve the data
+	api.serve()				// provide api
+}
