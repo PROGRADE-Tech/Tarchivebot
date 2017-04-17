1c3b40d4f428d672844b989a43b70cc816690aa5
fuerbringer <severin@protonmail.ch>
2017-04-17 17:45:44 +0200

    API for recent messages now provides first- and lastnames

--- libs/api/apibackend.js
+++ libs/api/apibackend.js
@@ -5,7 +5,11 @@ module.exports = {
   fetchLatest: function(key, amount, callback) {
     var data = []
     db.conn.all(
-      "SELECT message.id, message.data, message.timestamp, chat.name AS chat_name, user.name AS user_name, message_type.name AS message_type FROM message \
+      "SELECT message.id, message.data, message.timestamp, \
+        chat.name AS chat_name, user.name AS user_name, \
+        user.first_name AS first_name, user.last_name AS last_name, \
+        message_type.name AS message_type \
+      FROM message \
       JOIN chat ON message.chat_id=chat.id \
       JOIN user ON message.user_id=user.id \
       JOIN message_type ON message.message_type_id=message_type.id \
