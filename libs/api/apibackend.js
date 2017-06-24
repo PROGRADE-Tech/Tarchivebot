7fea59e79f49b3c698da22b5928d84d1b8e0ab2f
fuerbringer <severin@protonmail.ch>
2017-06-24 23:16:48 +0200

    Small improvements to API call validation to prevent crashing the app

--- libs/api/apibackend.js
+++ libs/api/apibackend.js
@@ -4,79 +4,87 @@ module.exports = {
 
   fetchLatest: function(key, amount, callback) {
     var data = []
-    const query = "SELECT message.id, message.data, message.timestamp, \
-        chat.name AS chat_name, user.name AS user_name, \
-        user.first_name AS first_name, user.last_name AS last_name, \
-        message_type.name AS message_type \
-      FROM message \
-      JOIN chat ON message.chat_id=chat.id \
-      JOIN user ON message.user_id=user.id \
-      JOIN message_type ON message.message_type_id=message_type.id \
-      WHERE chat_id=(SELECT id FROM chat WHERE api_key=?) \
-      ORDER BY timestamp DESC"
-    if(amount && !isNaN(amount)) {
-      // An amount limit was specified
-      const queryLimit = query + " LIMIT ?"
-      db.conn.all(queryLimit, key, amount, function(err, rows) {
-        rows.forEach(function(row) {
-          data.push(row)
-        })
-        callback(data)
-      })
-    } else {
-      // Return all the messeges we've got
-      db.conn.all(query, key, function(err, rows) {
-        rows.forEach(function(row) {
-          data.push(row)
-        })
-        callback(data)
-      })
-    }
-  },
 
-  searchForMessages: function(key, amount, str, callback) {
-    var data = []
-    const query = "SELECT message.id, message.data, message.timestamp, \
-        chat.name AS chat_name, user.name AS user_name, \
-        user.first_name AS first_name, user.last_name AS last_name, \
-        message_type.name AS message_type \
-      FROM message \
-      JOIN chat ON message.chat_id=chat.id \
-      JOIN user ON message.user_id=user.id \
-      JOIN message_type ON message.message_type_id=message_type.id \
-      WHERE chat_id=(SELECT id FROM chat WHERE api_key=?) AND \
-        (message.data LIKE ? OR \
-        message.data LIKE ? OR \
-        message.data LIKE ? OR \
-        message.data LIKE ?) \
-      ORDER BY timestamp DESC"
-      // LIMIT ?"
+    if(key) {
+      const query = "SELECT message.id, message.data, message.timestamp, \
+          chat.name AS chat_name, user.name AS user_name, \
+          user.first_name AS first_name, user.last_name AS last_name, \
+          message_type.name AS message_type \
+        FROM message \
+        JOIN chat ON message.chat_id=chat.id \
+        JOIN user ON message.user_id=user.id \
+        JOIN message_type ON message.message_type_id=message_type.id \
+        WHERE chat_id=(SELECT id FROM chat WHERE api_key=?) \
+        ORDER BY timestamp DESC"
       if(amount && !isNaN(amount)) {
-        // An amount was specified
-        const querylimit = query + " LIMIT ?"
-        db.conn.all(query, key, str, '%'+str, str+'%', '%'+str+'%', amount, function(err, rows) {
+        // An amount limit was specified
+        const queryLimit = query + " LIMIT ?"
+        db.conn.all(queryLimit, key, amount, function(err, rows) {
           rows.forEach(function(row) {
             data.push(row)
           })
           callback(data)
         })
       } else {
-        db.conn.all(query, key, str, '%'+str, str+'%', '%'+str+'%', function(err, rows) {
+        // Return all the messeges we've got
+        db.conn.all(query, key, function(err, rows) {
           rows.forEach(function(row) {
             data.push(row)
           })
           callback(data)
         })
       }
+    }
+  },
+
+  searchForMessages: function(key, amount, str, callback) {
+    var data = []
+    if(key && str && (typeof str === 'string' || str instanceof String)) {
+      const query = "SELECT message.id, message.data, message.timestamp, \
+          chat.name AS chat_name, user.name AS user_name, \
+          user.first_name AS first_name, user.last_name AS last_name, \
+          message_type.name AS message_type \
+        FROM message \
+        JOIN chat ON message.chat_id=chat.id \
+        JOIN user ON message.user_id=user.id \
+        JOIN message_type ON message.message_type_id=message_type.id \
+        WHERE chat_id=(SELECT id FROM chat WHERE api_key=?) AND \
+          (message.data LIKE ? OR message.data LIKE ? OR \
+          message.data LIKE ? OR  message.data LIKE ?) \
+        ORDER BY timestamp DESC"
+        if(amount && !isNaN(amount)) {
+          // An amount was specified
+          const queryLimit = query + " LIMIT ?"
+          db.conn.all(queryLimit, key, str, '%'+str, str+'%', '%'+str+'%', amount, function(err, rows) {
+            rows.forEach(function(row) {
+              data.push(row)
+            })
+            callback(data)
+          })
+        } else {
+          db.conn.all(query, key, str, '%'+str, str+'%', '%'+str+'%', function(err, rows) {
+            rows.forEach(function(row) {
+              data.push(row)
+            })
+            callback(data)
+          })
+        }
+      } else {
+        callback(data)
+      }
     },
 
     validateApiKey: function(key, callback) {
-      db.conn.all("SELECT rowid FROM chat WHERE name != '' AND api_key=? LIMIT 1", key, function(err, row) {
-        var status = false
-        if(row.length >= 1) {
-          status = true
-        }
-        callback([{'status': status}])
-      })
+      if(key) {
+        db.conn.all("SELECT rowid FROM chat WHERE name != '' AND api_key=? LIMIT 1", key, function(err, row) {
+          var status = false
+          if(row.length >= 1) {
+            status = true
+          }
+          callback([{'status': status}])
+        })
+      } else {
+        callback([])
+      }
     }
 }
