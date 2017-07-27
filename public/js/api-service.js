83386ca484cc53004b0eac44c70f387b7ae49919
fuerbringer <severin@protonmail.ch>
2017-07-27 14:45:44 +0200

    An attempt to fix indentation.

--- public/js/api-service.js
+++ public/js/api-service.js
@@ -3,40 +3,40 @@
  */
 
 var APIService = angular.module('APIService', [])
-.service('API', function ($http, $filter) {
-    console.log("APIService loaded");
+	.service('API', function ($http, $filter) {
+		console.log("APIService loaded");
 
-    this.recent = function (key, amount, callback) {
-      $http.post("/api/recent", {"key": key, "amount": amount})
-       .then(
-           function(response){
-             // success
-             for (var i = 0; i < response.data.length; i++) {
-               response.data[i].dateformatted = $filter('date')(response.data[i].timestamp * 1000, 'HH:mm:ss - dd/MM/yy');
-             }
-             callback(response.data);
-           },
-           function(response){
-             // fail
-             console.log(response);
-           }
-        );
+		this.recent = function (key, amount, callback) {
+			$http.post("/api/recent", {"key": key, "amount": amount})
+				.then(
+					function(response){
+						// success
+						for (var i = 0; i < response.data.length; i++) {
+							response.data[i].dateformatted = $filter('date')(response.data[i].timestamp * 1000, 'HH:mm:ss - dd/MM/yy');
+						}
+						callback(response.data);
+					},
+					function(response){
+						// fail
+						console.log(response);
+					}
+				);
 
-      return;
-    };
+			return;
+		};
 
 		this.validateKey = function(key, callback) {
-      $http.post("/api/validatekey", {"key": key})
-       .then(
-           function(response){
-             // success
-             callback(response.data);
-           },
-           function(response){
-             console.log(response);
-           }
-        );
+			$http.post("/api/validatekey", {"key": key})
+				.then(
+					function(response){
+						// success
+						callback(response.data);
+					},
+					function(response){
+						console.log(response);
+					}
+				);
 
-      return;
+			return;
 		}
-});
+	});
--- public/js/core.js
+++ public/js/core.js
@@ -1,62 +1,62 @@
 var tarchive = angular.module('tarchive', ["APIService", "ngTable", "ngSanitize"]);
 
 tarchive.config(['$compileProvider', function( $compileProvider ) {
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|http|data):/);
-  }
+	$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|http|data):/);
+}
 ]);
 
 tarchive.controller('CoreController', ['$scope', '$filter', 'API', 'NgTableParams', function($scope, $filter, API, NgTableParams) {
 
-  $scope.key = localStorage.getItem('tarchiveKey');
-  $scope.messageAmount = 1000;
-  $scope.tableData = [];
-  $scope.content = "table";
+	$scope.key = localStorage.getItem('tarchiveKey');
+	$scope.messageAmount = 1000;
+	$scope.tableData = [];
+	$scope.content = "table";
 
-  $scope.messageTable = new NgTableParams({
-        page: 1,            // show first page
-        count: 10           // count per page
-    }, {
-        total: $scope.tableData.length, // length of data
-        getData: function (params) {
+	$scope.messageTable = new NgTableParams({
+		page: 1,            // show first page
+		count: 10           // count per page
+	}, {
+		total: $scope.tableData.length, // length of data
+		getData: function (params) {
 
-            // use build-in angular filter
-            var orderedData = params.filter() ? $filter('filter')($scope.tableData, params.filter()) : $scope.tableData;
-            params.total(orderedData.length);
+			// use build-in angular filter
+			var orderedData = params.filter() ? $filter('filter')($scope.tableData, params.filter()) : $scope.tableData;
+			params.total(orderedData.length);
 
-            var sorted = $filter('orderBy')(orderedData, params.orderBy());
+			var sorted = $filter('orderBy')(orderedData, params.orderBy());
 
-            var paged = sorted.slice((params.page() - 1) * params.count(), params.page() * params.count());
+			var paged = sorted.slice((params.page() - 1) * params.count(), params.page() * params.count());
 
-            return paged;
-        }
-    });
+			return paged;
+		}
+	});
 
-    $scope.getRecentMessages = function() {
-      API.recent($scope.key, $scope.messageAmount, function(data) {
-        $scope.tableData = data;
-        $scope.messageTable.reload();
-        $scope.setJsonExportData();
-      });
-    };
+	$scope.getRecentMessages = function() {
+		API.recent($scope.key, $scope.messageAmount, function(data) {
+			$scope.tableData = data;
+			$scope.messageTable.reload();
+			$scope.setJsonExportData();
+		});
+	};
 
 
-    $scope.askForKey = function() {
-      swal({
-        title: "Enter your key!",
-        text: "Enter the key provided by the bot:",
-        type: "input",
-        showCancelButton: true,
-        closeOnConfirm: false,
-        animation: true,
-        inputPlaceholder: "Key"
-      },
-      function(inputValue){
-        if (inputValue === false) return false;
+	$scope.askForKey = function() {
+		swal({
+			title: "Enter your key!",
+			text: "Enter the key provided by the bot:",
+			type: "input",
+			showCancelButton: true,
+			closeOnConfirm: false,
+			animation: true,
+			inputPlaceholder: "Key"
+		},
+			function(inputValue){
+				if (inputValue === false) return false;
 
-        if (inputValue === "") {
-          swal.showInputError("You need to write something!");
-          return false
-        }
+				if (inputValue === "") {
+					swal.showInputError("You need to write something!");
+					return false
+				}
 
 				API.validateKey(inputValue, function(data) {
 					if(data.status === true) {
@@ -72,30 +72,30 @@ tarchive.controller('CoreController', ['$scope', '$filter', 'API', 'NgTableParam
 							html: true
 						});
 					} else {
-          	swal.showInputError("Oops! That doesn't appear to be a valid key.");
-          	return false
+						swal.showInputError("Oops! That doesn't appear to be a valid key.");
+						return false
 					}
 				});
-      });
-    };
+			});
+	};
 
-    $scope.setJsonExportData = function() {
-      var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify($scope.tableData));
-      $scope.jsonData = dataStr;
-      $scope.jsonDataFile = Date.now() + "_tarchive_table.json";
+	$scope.setJsonExportData = function() {
+		var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify($scope.tableData));
+		$scope.jsonData = dataStr;
+		$scope.jsonDataFile = Date.now() + "_tarchive_table.json";
 
-      if($scope.tableData && $scope.tableData.length) {
-        $scope.exportButtonClass = "btn-export";
-      } else {
-        $scope.exportButtonClass = "invisible";
-      }
-    };
+		if($scope.tableData && $scope.tableData.length) {
+			$scope.exportButtonClass = "btn-export";
+		} else {
+			$scope.exportButtonClass = "invisible";
+		}
+	};
 
-    if ($scope.key === null) {
-      $scope.askForKey();
-      $scope.exportButtonClass = "invisible";
-    } else {
-      $scope.getRecentMessages();
-    }
+	if ($scope.key === null) {
+		$scope.askForKey();
+		$scope.exportButtonClass = "invisible";
+	} else {
+		$scope.getRecentMessages();
+	}
 
 }]);
