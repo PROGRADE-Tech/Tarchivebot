4307c4f42b16cc45f8ee3118c617f26cdbe59742
Severin Fürbringer <severin@protonmail.ch>
2017-08-19 12:31:50 +0200

    Selfhosted dependencies (#65)
    * Selfhosted and minified JS dependencies.
    
    * Correct grunt include for bootstrap JS.
    
    * Selfhosted and minified CSS files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,12 @@ module.exports = function (grunt) {
       static_js: {
         files: {
           'public/js/min/master.min.js': [
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/angular/angular.min.js',
+            'node_modules/angular-sanitize/angular-sanitize.min.js',
+            'node_modules/ng-table/bundles/ng-table.min.js',
+            'node_modules/sweetalert/dist/sweetalert.min.js',
             'public/js/api-service.js',
             'public/js/core.js'
           ]
@@ -23,10 +29,27 @@ module.exports = function (grunt) {
         // Play nice with Angular:
         mangle: false
       }
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'public/css/min/master.min.css': [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/ng-table/bundles/ng-table.min.css',
+            'node_modules/sweetalert/dist/sweetalert.css',
+            'public/css/main.css'
+          ]
+        }
+      }
     }
   })
 
   grunt.loadNpmTasks('grunt-contrib-watch')
   grunt.loadNpmTasks('grunt-contrib-uglify')
-  grunt.registerTask('default', ['uglify', 'watch'])
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.registerTask('default', ['uglify', 'cssmin', 'watch'])
 }
